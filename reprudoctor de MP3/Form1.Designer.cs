5a6d978049ad69862a1afdfb3761576856156507
Alumno <Alumno@PC-44>
2026-09-22 08:34:49 -0600

    Agregar archivos de proyecto.

---
+++ reprudoctor de MP3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace reprudoctor_de_MP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            btnPlay = new Button();
+            btnStop = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(306, 130);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(160, 106);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // btnPlay
+            // 
+            btnPlay.BackColor = Color.Chartreuse;
+            btnPlay.Location = new Point(193, 283);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(105, 40);
+            btnPlay.TabIndex = 1;
+            btnPlay.Text = "Play";
+            btnPlay.UseVisualStyleBackColor = false;
+            btnPlay.Click += button1_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.BackColor = Color.Chocolate;
+            btnStop.Location = new Point(481, 283);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(92, 40);
+            btnStop.TabIndex = 2;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(65, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(120, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Archivo seleccionado";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 15);
+            label2.TabIndex = 4;
+            label2.Text = "No hay ningun archivo seleccionado";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnStop);
+            Controls.Add(btnPlay);
+            Controls.Add(pictureBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnPlay;
+        private Button btnStop;
+        private Label label1;
+        private Label label2;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ reprudoctor de MP3/Form1.cs
@@ -0,0 +1,55 @@
+using WMPLib;
+namespace reprudoctor_de_MP3
+{
+    public partial class Form1 : Form
+    {
+        //objeto que se encarga de reproducir el audio
+
+        private WindowsMediaPlayer reproductor;
+        // guardar la ruta del archivo selecciionado
+        private string archivoSeleccionado = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            //creamos reproductor
+            reproductor = new WindowsMediaPlayer();
+            //evita a reproducir audio
+            reproductor.settings.autoStart = false;
+            // configuramos el openFileDIalog
+            openFileDialog1.Filter =
+                "Archivo de audio (*.mp3)|*.mp3";
+            openFileDialog1.Title = "selecciona un archivo mp3";
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //si no hay un archivo selecionado 
+                //Abrimos el explorador de archivo
+                if (string.IsNullOrEmpty(archivoSeleccionado))
+                {
+                    DialogResult resultado =
+                        openFileDialog1.ShowDialog();
+                    if (resultado != DialogResult.OK)
+                    {
+                        archivoSeleccionado = openFileDialog1.FileName;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            }
+    }
+}
